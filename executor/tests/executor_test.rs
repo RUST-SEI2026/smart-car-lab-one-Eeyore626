c863a1cfdc3537ef9d948baa93b356e74b0ba4e8
Eeyore626 <1078113917@qq.com>
2026-04-25 00:17:59 +0800

    add test case:should_return_x_minus_1_given_command_is_m_and_facing_is_w

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -134,7 +134,19 @@ mod move_tests {
         assert_eq!(expected_pose, executor.query());
     }
 
-    
+    #[test]
+    fn should_return_x_minus_1_given_command_is_m_and_facing_is_w() {
+        // given
+        let original_pose = Pose::new(0, 0, 'W');
+        let mut executor = Executor::with_pose(original_pose);
+
+        // when
+        executor.execute("M");
+
+        // then
+        let expected_pose = Pose::new(-1, 0, 'W');
+        assert_eq!(expected_pose, executor.query());
+    }
 
    
 
