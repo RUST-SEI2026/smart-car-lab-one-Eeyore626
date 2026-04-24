2b3ce8a22803b9c21cac792675f314be13fbbe05
eeyore <1078113917@qq.com>
2026-04-24 23:46:39 +0800

    add test case: should_return_facing_e_given_command_is_l_and_facing_is_s

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -16,6 +16,19 @@ mod turn_left_tests {
         assert_eq!(expected_pose, executor.query());
     }
 
+    #[test]
+    fn should_return_facing_e_given_command_is_l_and_facing_is_s() {
+        // given
+        let original_pose = Pose::new(0, 0, 'S');
+        let mut executor = Executor::with_pose(original_pose);
+
+        // when
+        executor.execute("L");
+
+        // then
+        let expected_pose = Pose::new(0, 0, 'E');
+        assert_eq!(expected_pose, executor.query());
+    }
 
 
 
