8e59cc6fad5da129c2bde27aa10984ef08674e71
Eeyore626 <1078113917@qq.com>
2026-04-24 23:58:43 +0800

    add test case:should_return_facing_s_given_command_is_r_and_facing_is_e

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -58,3 +58,23 @@ mod turn_left_tests {
         assert_eq!(expected_pose, executor.query());
     }
 }
+mod turn_right_tests {
+    use super::*;
+
+    #[test]
+    fn should_return_facing_s_given_command_is_r_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+
+        // when
+        executor.execute("R");
+
+        // then
+        let expected_pose = Pose::new(0, 0, 'S');
+        assert_eq!(expected_pose, executor.query());
+    }
+
+
+   
+}
