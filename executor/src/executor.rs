9320638ebb146549b7d1e2e60c8233c306d5e606
Eeyore626 <1078113917@qq.com>
2026-04-25 00:20:40 +0800

    pass test case:should_return_y_minus_1_given_command_is_m_and_facing_is_s

--- executor/src/executor.rs
+++ executor/src/executor.rs
@@ -37,7 +37,7 @@ impl Executor {
                     'E' => self.pose.x += 1,
                     'S' => self.pose.y -= 1,
                     'W' => self.pose.x -= 1,
-                 
+                    'N' => self.pose.y += 1,
                     _ => (),
                 },
                 'L' => match self.pose.heading {
